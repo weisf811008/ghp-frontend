c22893056a3ca6fe61a7bded93eeb7e35f966342
Weisf <snow811008@gmail.com>
2022-09-26 00:47:27 +0800

    feat: rename.

---
+++ src/stores/adminSchool.js
@@ -0,0 +1,51 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { request } from '../utils/http.util'
+
+const resource = 'admin/schools'
+
+export const useAdminSchoolStore = defineStore(resource, () => {
+  const schools = ref([])
+  const isLoading = ref(false)
+
+  //取得學校資料清單
+  const getSchools = async () => {
+    isLoading.value = true
+    const res = await request.get(resource)
+    schools.value = res.data
+    isLoading.value = false
+  }
+
+  //新增一筆學校資料
+  const createSchool = async (data) => {
+    isLoading.value = true
+    await request.post(resource, data)
+    isLoading.value = false
+    await getSchools()
+  }
+
+  //修改一筆學校資料
+  const updateSchool = async (id, data) => {
+    isLoading.value = true
+    await request.put(`${resource}/${id}`, data)
+    isLoading.value = false
+    await getSchools()
+  }
+
+  //刪除一筆學校資料
+  const deleteSchool = async (id) => {
+    isLoading.value = true
+    await request.delete(`${resource}/${id}`)
+    isLoading.value = false
+    await getSchools()
+  }
+
+  return {
+    schools,
+    isLoading,
+    getSchools,
+    createSchool,
+    updateSchool,
+    deleteSchool,
+  }
+})
---
+++ src/stores/adminUsers.js
@@ -0,0 +1,68 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { request } from '../utils/http.util'
+
+const resource = 'admin/users'
+
+export const useAdminUserStore = defineStore(resource, () => {
+  const adminUsers = ref([])
+  const isLoading = ref(false)
+
+  //取得帳號清單
+  const getAdminUsers = async () => {
+    isLoading.value = true
+    const res = await request.get(resource)
+    adminUsers.value = res.data
+    isLoading.value = false
+  }
+
+  //新增一個帳號
+  const createAdminUser = async (data) => {
+    isLoading.value = true
+    await request.post(resource, data)
+    isLoading.value = false
+    await getAdminUsers()
+  }
+
+  //取得一個帳號
+  const getAdminUser = async (id) => {
+    isLoading.value = true
+    const res = await request.get(`${resource}/${id}`)
+    isLoading.value = false
+    return res.data
+  }
+
+  //修改一個帳號
+  const updateAdminUser = async (id, data) => {
+    isLoading.value = true
+    await request.put(`${resource}/${id}`, data)
+    isLoading.value = false
+    await getAdminUsers()
+  }
+
+  //刪除一個帳號
+  const deleteAdminUser = async (id) => {
+    isLoading.value = true
+    await request.delete(`${resource}/${id}`)
+    isLoading.value = false
+    await getAdminUsers()
+  }
+
+  //重設一個帳號的密碼
+  const resetAdminPassword = async (id, password) => {
+    isLoading.value = true
+    await request.patch(`${resource}/${id}/password`, { password })
+    isLoading.value = false
+  }
+
+  return {
+    adminUsers,
+    isLoading,
+    getAdminUsers,
+    getAdminUser,
+    createAdminUser,
+    updateAdminUser,
+    deleteAdminUser,
+    resetAdminPassword,
+  }
+})
