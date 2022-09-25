7ad3a07089ee62d5404f9782d9dd369c71b3174c
Weisf <snow811008@gmail.com>
2022-09-26 00:50:51 +0800

    feat: component rename.

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { useAuthStore } from '../stores/auth'
 import axios from 'axios'
-import LoginLayout from '../layouts/AppLoginLayout.vue'
+import { ElNotification } from 'element-plus'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -11,106 +11,111 @@ const router = createRouter({
       path: '/auth/login',
       name: 'Login',
       component: () => import('../views/PageLogin.vue'),
-      meta: { layout: LoginLayout }
+      meta: { layout: 'Login' },
     },
     {
       path: '/home',
       name: 'Home',
-      component: () => import('../views/PageHome.vue')
+      component: () => import('../views/PageHome.vue'),
     },
     {
       path: '/settings/regulations',
       name: 'SettingRegulations',
-      component: () => import('../views/PageSettingRegulations.vue')
+      component: () => import('../views/PageSettingRegulations.vue'),
     },
     {
-      path: '/settings/visits',
+      path: '/settings/visitingForms',
       name: 'SettingVisits',
-      component: () => import('../views/PageSettingVisits.vue')
+      component: () => import('../views/PageSettingVisitingForms.vue'),
     },
     {
       path: '/settings/categories',
       name: 'SettingCategories',
-      component: () => import('../views/PageSettingCategories.vue')
+      component: () => import('../views/PageSettingCategories.vue'),
     },
     {
       path: '/settings/items',
       name: 'SettingItems',
-      component: () => import('../views/PageSettingItems.vue')
+      component: () => import('../views/PageSettingItems.vue'),
     },
     {
       path: '/forms',
       name: 'Forms',
-      component: () => import('../views/PageForms.vue')
+      component: () => import('../views/PageForms.vue'),
     },
     {
       path: '/inspection',
       name: 'Inspection',
-      component: () => import('../views/PageInspection.vue')
+      component: () => import('../views/PageInspection.vue'),
     },
     {
       path: '/inspection/submitting',
       name: 'InspectionSubmitting',
-      component: () => import('../views/PageInspectionSubmitting.vue')
+      component: () => import('../views/PageInspectionSubmitting.vue'),
     },
-    {
-      path: '/inspection/correcting/:id',
-      name: 'InspectionCorrecting',
-      params: true,
-      component: () => import('../views/PageInspectionCorrecting.vue')
-    },
+    // {
+    //   path: '/inspection/correcting/:id',
+    //   name: 'InspectionCorrecting',
+    //   params: true,
+    //   component: () => import('../views/PageInspectionCorrecting.vue'),
+    // },
     {
       path: '/reports/daily',
-      name: 'Reports./daily',
-      component: () => import('../views/PageReportsDaily.vue')
+      name: 'ReportsDaily',
+      component: () => import('../views/PageReportsDaily.vue'),
+    },
+    {
+      path: '/reports/prodtemp',
+      name: 'ReportsProdTemp',
+      component: () => import('../views/PageReportsProdTemp.vue'),
+    },
+    {
+      path: '/reports/tnh',
+      name: 'ReportsTnh',
+      component: () => import('../views/PageReportsTnh.vue'),
+    },
+    {
+      path: '/reports/tableware',
+      name: 'ReportsTableware',
+      component: () => import('../views/PageReportsTableware.vue'),
     },
     {
       path: '/reports/ghp',
-      name: 'Reports./ghp',
-      component: () => import('../views/PageReportsGhp.vue')
+      name: 'ReportsGhp',
+      component: () => import('../views/PageReportsGhp.vue'),
     },
     {
-      path: '/reports/visit',
-      name: 'Reports./visit',
-      component: () => import('../views/PageReportsVisit.vue')
+      path: '/reports/visiting',
+      name: 'ReportsVisiting',
+      component: () => import('../views/PageReportsVisiting.vue'),
     },
     {
-      path: '/reports/productTemperature',
-      name: 'Reports./productTemperature',
-      component: () => import('../views/PageReportsProductTemperature.vue')
+      path: '/settings/admin/schools',
+      name: 'SettingAdminSchools',
+      component: () => import('../views/PageSettingAdminSchools.vue'),
     },
     {
-      path: '/reports/temperatureAndHumidity',
-      name: 'Reports./temperatureAndHumidity',
-      component: () => import('../views/PageReportsTemperatureAndHumidity.vue')
-    },    
-    {
-      path: '/reports/tablewareInspection',
-      name: 'Reports./tablewareInspection',
-      component: () => import('../views/PageReportsTablewareInspection.vue')
+      path: '/settings/admin/users',
+      name: 'SettingAdminUsers',
+      component: () => import('../views/PageSettingAdminUsers.vue'),
     },
-    // {
-    //   path: '/settings/schools',
-    //   name: 'SettingSchools',
-    //   component: () => import('../views/PageSettingSchools.vue')
-    // },
     {
       path: '/settings/users',
       name: 'SettingUsers',
-      component: () => import('../views/PageSettingUsers.vue')
+      component: () => import('../views/PageSettingUsers.vue'),
     },
     {
       path: '/history',
       name: 'History',
-      component: () => import('../views/PageHistory.vue')
+      component: () => import('../views/PageHistory.vue'),
     },
     {
       path: '/:pathMatch(.*)*',
       redirect: {
-        name: 'Home'
-      }
-    }
-  ]
+        name: 'Home',
+      },
+    },
+  ],
 })
 
 // redirect to login page if not logged in and trying to access a restricted page
@@ -122,16 +127,15 @@ router.beforeEach(async (to, from, next) => {
   const { user } = storeToRefs(authStore)
 
   try {
-    await authStore.getUserInfo()
-  } catch (e) {
-    if (!axios.isAxiosError(e) || e.response.status !== 401) {
-      console.log(e)
+    if (authRequired) {
+      await authStore.getUserInfo()
     }
-  }
+  } catch (e) {}
 
   if (authRequired && !(user.value && user.value.username)) {
     authStore.setReturnUrl(to.fullPath)
-    router.push({ name: 'Login' })
+    next({ name: 'Login' })
+    return
   }
   next()
 })
