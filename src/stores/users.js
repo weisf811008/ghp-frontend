5409d0646ba5f1017a8fb7281433935a24440a28
Weisf <snow811008@gmail.com>
2022-09-26 14:46:12 +0800

    feat: comment.

--- src/stores/users.js
+++ src/stores/users.js
@@ -8,6 +8,7 @@ export const useUserStore = defineStore(resource, () => {
   const users = ref([])
   const isLoading = ref(false)
 
+  //取得帳號清單
   const getUsers = async () => {
     isLoading.value = true
     const res = await request.get(resource)
@@ -15,6 +16,7 @@ export const useUserStore = defineStore(resource, () => {
     isLoading.value = false
   }
 
+  //新增一個帳號
   const createUser = async (data) => {
     isLoading.value = true
     await request.post(resource, data)
@@ -22,6 +24,7 @@ export const useUserStore = defineStore(resource, () => {
     await getUsers()
   }
 
+  //修改一個帳號
   const updateUser = async (id, data) => {
     isLoading.value = true
     await request.put(`${resource}/${id}`, data)
@@ -29,6 +32,7 @@ export const useUserStore = defineStore(resource, () => {
     await getUsers()
   }
 
+  //刪除一個帳號
   const deleteUser = async (id) => {
     isLoading.value = true
     await request.delete(`${resource}/${id}`)
@@ -36,6 +40,7 @@ export const useUserStore = defineStore(resource, () => {
     await getUsers()
   }
 
+  //重設一個帳號的密碼
   const resetPassword = async (id, password) => {
     isLoading.value = true
     await request.patch(`${resource}/${id}/password`, { password })
