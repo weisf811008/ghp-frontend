e34bccb7a526978a6827aa2e56d51372a2a89974
Weisf <snow811008@gmail.com>
2022-10-09 21:55:41 +0800

    refactor> regulations page.

--- src/components/regulation/AppRegulationUpdateDialog.vue
+++ src/components/regulation/AppRegulationUpdateDialog.vue
@@ -1,60 +1,62 @@
 <template>
-  <el-dialog v-model="isShow" title="修改GHP條文" @open="handleOpenDialog">
-    <el-form
-      ref="formRef"
-      :model="newData"
-      :rules="rules"
-      :disabled="isLoading"
-      size="large"
-      label-width="auto"
-      status-icon
-      hide-required-asterisk
-    >
-      <el-form-item label="類別序號" prop="code">
-        {{ newData.code }}
-      </el-form-item>
-      <el-form-item label="類別" prop="class">
-        <el-select
-          class="formSelect"
-          size="large"
-          v-model="newData.class"
-          placeholder="選擇類別"
-        >
-          <el-option label="食品業者一般性規定" value="食品業者一般性規定" />
-          <el-option
-            label="餐飲與即時熟食業者專業性規定"
-            value="餐飲與即時熟食業者專業性規定"
+  <div>
+    <el-dialog v-model="isShow" title="修改GHP條文" @open="handleOpenDialog">
+      <el-form
+        ref="formRef"
+        :model="newData"
+        :rules="rules"
+        :disabled="isLoading"
+        size="large"
+        label-width="auto"
+        status-icon
+        hide-required-asterisk
+      >
+        <el-form-item label="類別序號" prop="code">
+          {{ newData.code }}
+        </el-form-item>
+        <el-form-item label="類別" prop="class">
+          <el-select
+            class="formSelect"
+            size="large"
+            v-model="newData.class"
+            placeholder="選擇類別"
+          >
+            <el-option label="食品業者一般性規定" value="食品業者一般性規定" />
+            <el-option
+              label="餐飲與即時熟食業者專業性規定"
+              value="餐飲與即時熟食業者專業性規定"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="食品良好衛生規範法規GHP檢查" prop="description">
+          <el-input
+            v-model="newData.description"
+            rows="5"
+            type="textarea"
+            placeholder="請輸入食品良好衛生規範法規GHP檢查內容"
           />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="食品良好衛生規範法規GHP檢查" prop="description">
-        <el-input
-          v-model="newData.description"
-          rows="5"
-          type="textarea"
-          placeholder="請輸入食品良好衛生規範法規GHP檢查內容"
-        />
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button
-          type="danger"
-          text
-          size="large"
-          class="cancel-button"
-          @click.prevent="closeDialog"
-          >取消
-        </el-button>
-        <el-button
-          type="primary"
-          size="large"
-          @click.prevent="() => updateRegulation(formRef)"
-          >送出
-        </el-button>
-      </span>
-    </template>
-  </el-dialog>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button
+            type="danger"
+            text
+            size="large"
+            class="cancel-button"
+            @click.prevent="closeDialog"
+            >取消
+          </el-button>
+          <el-button
+            type="primary"
+            size="large"
+            @click.prevent="() => updateRegulation(formRef)"
+            >送出
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 <script setup>
 import { ref, computed } from 'vue'
