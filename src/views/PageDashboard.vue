3c2f61ff1475a798612f5e2789bf51605438b6ab
Weisf <snow811008@gmail.com>
2022-10-25 23:01:13 +0800

    feat: added empty view

--- src/views/PageDashboard.vue
+++ src/views/PageDashboard.vue
@@ -8,43 +8,37 @@
     </template>
     <div class="row">
       <div class="chart">
-        <h3>檢核大項不合格數</h3>
-        <AppPieChart v-loading="isLoading" :chartData="abnormalCategories" />
+        <h3>檢核大項缺失次數</h3>
+        <AppPieChart
+          v-show="!abnormalCategoriesIsEmpty"
+          v-loading="isLoading"
+          :chartData="abnormalCategories"
+        />
+        <el-empty v-show="abnormalCategoriesIsEmpty" description="No data" />
       </div>
       <div class="chart">
-        <h3>GHP條文十大不合格</h3>
-        <AppBarChart
+        <h3>檢核大項合格次數</h3>
+        <AppPieChart
+          v-show="!normalCategoriesIsEmpty"
           v-loading="isLoading"
-          :chartData="abnormalGhp"
-          :width="200"
-          :height="100"
+          :chartData="normalCategories"
         />
+        <el-empty v-show="normalCategoriesIsEmpty" description="No data" />
       </div>
     </div>
-    <div class="row">
-      <div class="chart">
-        <h3>檢核細項十大不合格</h3>
-        <AppLineChart
-          v-loading="isLoading"
-          :chartData="abnormalItems"
-          :width="200"
-          :height="100"
-        />
-      </div>
-      <div class="chart">
-        <h3>訪視表條文十大不合格</h3>
-        <AppBarChart
-          v-loading="isLoading"
-          :chartData="abnormalVisiting"
-          :width="200"
-          :height="100"
-        />
-      </div>
+    <div>
+      <h3>檢核細項十大缺失</h3>
+      <AppBarChart
+        v-show="!abnormalItemsIsEmpty"
+        v-loading="isLoading"
+        :chartData="abnormalItems"
+      />
+      <el-empty v-show="abnormalItemsIsEmpty" description="No data" />
     </div>
   </el-card>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { subDays, startOfDay } from 'date-fns'
 import { storeToRefs } from 'pinia'
 import { useChartStore } from '../stores/charts'
@@ -54,60 +48,84 @@ import AppLineChart from '../components/charts/AppLineChart.vue'
 
 const chartStore = useChartStore()
 const { isLoading } = storeToRefs(chartStore)
+const { getNormalCategories, getAbnormalCategories, getAbnormalItems } =
+  chartStore
 
 //date picker
 const dateRange = ref([])
 const today = startOfDay(new Date())
-const dates = ref([subDays(today, 6), today])
+const dates = ref([subDays(today, 13), today])
 
-const loaded = ref(false)
+const normalCategoriesIsEmpty = ref(false)
+const abnormalCategoriesIsEmpty = ref(false)
+const abnormalItemsIsEmpty = ref(false)
 
+const normalCategories = ref({})
 const abnormalCategories = ref({})
 const abnormalItems = ref({})
-const abnormalGhp = ref({})
-const abnormalVisiting = ref({})
 
-const getAbnormalCategories = async () => {
-  abnormalCategories.value = await chartStore.getAbnormalCategories(
+const getNormalCategoriesData = async () => {
+  normalCategories.value = await getNormalCategories(
     dates.value[0],
     dates.value[1]
   )
 }
 
-const getAbnormalItems = async () => {
-  abnormalItems.value = await chartStore.getAbnormalItems(
+const getAbnormalCategoriesData = async () => {
+  abnormalCategories.value = await getAbnormalCategories(
     dates.value[0],
     dates.value[1]
   )
 }
 
-const getAbnormalGhp = async () => {
-  abnormalGhp.value = await chartStore.getAbnormalGhp(
-    dates.value[0],
-    dates.value[1]
-  )
-}
-
-const getAbnormalVisiting = async () => {
-  abnormalVisiting.value = await chartStore.getAbnormalVisiting(
-    dates.value[0],
-    dates.value[1]
-  )
+const getAbnormalItemsData = async () => {
+  abnormalItems.value = await getAbnormalItems(dates.value[0], dates.value[1])
 }
 
 onMounted(async () => {
-  await getAbnormalCategories()
-  await getAbnormalItems()
-  await getAbnormalGhp()
-  await getAbnormalVisiting()
+  await getNormalCategoriesData()
+  await getAbnormalCategoriesData()
+  await getAbnormalItemsData()
 })
 
 const handleChange = () => {
-  getAbnormalCategories()
-  getAbnormalItems()
-  getAbnormalGhp()
-  getAbnormalVisiting()
+  getNormalCategoriesData()
+  getAbnormalCategoriesData()
+  getAbnormalItemsData()
 }
+
+watch(
+  () => normalCategories.value,
+  (newValue) => {
+    if (newValue.datasets.length === 0) {
+      return (normalCategoriesIsEmpty.value = true)
+    } else {
+      return (normalCategoriesIsEmpty.value = false)
+    }
+  }
+)
+
+watch(
+  () => abnormalCategories.value,
+  (newValue) => {
+    if (newValue.datasets.length === 0) {
+      return (abnormalCategoriesIsEmpty.value = true)
+    } else {
+      return (abnormalCategoriesIsEmpty.value = false)
+    }
+  }
+)
+
+watch(
+  () => abnormalItems.value,
+  (newValue) => {
+    if (newValue.datasets.length === 0) {
+      return (abnormalItemsIsEmpty.value = true)
+    } else {
+      return (abnormalItemsIsEmpty.value = false)
+    }
+  }
+)
 </script>
 <style lang="scss" scoped>
 .row {
