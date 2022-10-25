73cf7df9a6529ed497dd72688ad3f6998bf4683c
Claire <45916818+weisf811008@users.noreply.github.com>
2022-10-26 01:34:16 +0800

    feat: added empty view (#11)

--- src/stores/charts.js
+++ src/stores/charts.js
@@ -7,6 +7,25 @@ const resource = 'charts'
 export const useChartStore = defineStore(resource, () => {
   const isLoading = ref(false)
 
+  //取得指定日期區間內合格大項發生次數
+  const getNormalCategories = async (start, end) => {
+    try {
+      isLoading.value = true
+      const res = await request.get(`${resource}/normalCategories`, {
+        params: {
+          start,
+          end,
+        },
+      })
+      return res.data
+    } catch (e) {
+      throw e
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  //取得指定日期區間內缺失大項發生次數
   const getAbnormalCategories = async (start, end) => {
     try {
       isLoading.value = true
@@ -24,6 +43,7 @@ export const useChartStore = defineStore(resource, () => {
     }
   }
 
+  //取得指定日期區間內十大缺失細項發生次數
   const getAbnormalItems = async (start, end) => {
     try {
       isLoading.value = true
@@ -41,45 +61,48 @@ export const useChartStore = defineStore(resource, () => {
     }
   }
 
-  const getAbnormalGhp = async (start, end) => {
-    try {
-      isLoading.value = true
-      const res = await request.get(`${resource}/abnormalGhp`, {
-        params: {
-          start,
-          end,
-        },
-      })
-      return res.data
-    } catch (e) {
-      throw e
-    } finally {
-      isLoading.value = false
-    }
-  }
+  //取得指定日期區間內十大缺失條文發生次數
+  // const getAbnormalGhp = async (start, end) => {
+  //   try {
+  //     isLoading.value = true
+  //     const res = await request.get(`${resource}/abnormalGhp`, {
+  //       params: {
+  //         start,
+  //         end,
+  //       },
+  //     })
+  //     return res.data
+  //   } catch (e) {
+  //     throw e
+  //   } finally {
+  //     isLoading.value = false
+  //   }
+  // }
 
-  const getAbnormalVisiting = async (start, end) => {
-    try {
-      isLoading.value = true
-      const res = await request.get(`${resource}/abnormalVisiting`, {
-        params: {
-          start,
-          end,
-        },
-      })
-      return res.data
-    } catch (e) {
-      throw e
-    } finally {
-      isLoading.value = false
-    }
-  }
+  //取得指定日期區間內十大缺失訪視表發生次數
+  // const getAbnormalVisiting = async (start, end) => {
+  //   try {
+  //     isLoading.value = true
+  //     const res = await request.get(`${resource}/abnormalVisiting`, {
+  //       params: {
+  //         start,
+  //         end,
+  //       },
+  //     })
+  //     return res.data
+  //   } catch (e) {
+  //     throw e
+  //   } finally {
+  //     isLoading.value = false
+  //   }
+  // }
 
   return {
     isLoading,
+    getNormalCategories,
     getAbnormalCategories,
     getAbnormalItems,
-    getAbnormalGhp,
-    getAbnormalVisiting,
+    // getAbnormalGhp,
+    // getAbnormalVisiting,
   }
 })
